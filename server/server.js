55cbde2aeed23e5f8ddc4de7c5d8d32834e4d373
Pathik <pathikdevani@gmail.com>
2022-10-10 12:43:13 +0400

    added gzip

--- server/server.js
+++ server/server.js
@@ -13,6 +13,30 @@ app.use(
   express.static(path.resolve(__dirname, "../html-webpack-handlebars/build"))
 );
 
+const gzipMime = {
+  html: "text/html",
+};
+app.use(
+  "/gz-webpack",
+  (req, res, next) => {
+    let last = req.url.split("/").pop();
+    if (last === "") {
+      last = "index.html";
+      req.url = req.url + last;
+    }
+
+    const mimeType = last.split(".").pop();
+    if (gzipMime[mimeType]) {
+      req.url = req.url + '.gz';
+      res.set("Content-Encoding", "gzip");
+      res.set("Content-Type", gzipMime[mimeType]);
+    }
+    next();
+  },
+
+  express.static(path.resolve(__dirname, "../html-webpack-handlebars/build/gz"))
+);
+
 app.use(
   "/react",
   express.static(path.resolve(__dirname, "../react-typescript/build"))
